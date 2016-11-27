04d709d9d7a21c953d92452957c397269555a658
Avand Amiri <avand@avandamiri.com>
2016-11-27 00:27:18 -0800

    Listing channels on /channels

--- server.rb
+++ server.rb
@@ -8,3 +8,11 @@ Dotenv.load
 get "/" do
   erb :index
 end
+
+get "/channels" do
+  client = Slack::Client.new token: ENV["SLACK_TOKEN"]
+
+  @channels = client.channels_list["channels"]
+
+  erb :channels
+end
